2e6c88851caf5ccd700f5323457faebc434e2211
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-03-19 15:32:03 +0100

    naardedb

--- felix.py
+++ felix.py
@@ -1,5 +1,30 @@
 print("in bestand van felix")
+import mysql.connector
 
 def methodevanfelix():
-
+    print("op naar de database")
     return "Dit komt van de methode van Felix"
+
+def tweedemethodevanfelix():
+    mydb = mysql.connector.connect(
+        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
+        user="yc2403admin",
+        password="abcd1234ABCD!@#$",
+        database="demopythondag"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM ober")
+
+    myresult = mycursor.fetchall()
+    eindstring = ""
+    for x in myresult:
+        print(x[1], " - ", x[3])
+        eindstring += x[1]+", "
+    return "yes: " + eindstring
+
+    
+
+
+print(tweedemethodevanfelix())
