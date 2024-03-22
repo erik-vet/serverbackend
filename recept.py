3fab2428f1395df07369989d9fac7c5cc78d1a54
Sjaakbob <mikevink1991@gmail.com>
2024-03-22 11:48:54 +0100

    a

--- recept.py
+++ recept.py
@@ -4,7 +4,7 @@ def allesSelecterenMethode():
     print("op naar de database")
     return "Dit komt van de methode van recpt allesSelecteren"
 
-def tweedemethodevanfelix():
+def tweedeAllesSelecterenMethode():
     mydb = mysql.connector.connect(
         host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
         user="yc2403admin",
