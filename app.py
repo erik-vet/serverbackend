0364fcb5d13a34c11e062cb4f34653177d2c1803
erik-vet <erikvet1@gmail.com>
2024-03-22 13:09:24 +0100

    tweede

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_cors import CORS
 
+import erik
 import felix
 import dominique
 
@@ -28,3 +29,7 @@ def methodefelix3(variabele1):
 @app.route("/dominique")
 def methodedominique():
     return dominique.open_youtube_link('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+@app.route("/erik1/<eenword>/<tweeword>")
+def methodeerik(eenword, tweeword):
+  return erik.methodeerik(eenword, tweeword)
