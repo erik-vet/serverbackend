15b86d30e67bae5b2554a7d8e031eada14d6f1ff
Sjaakbob <mikevink1991@gmail.com>
2024-03-22 11:35:39 +0100

    recept.py

---
+++ recept.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+def allesSelecterenMethode():
+    print("op naar de database")
+    return "Dit komt van de methode van recpt allesSelecteren"
+
+def tweedemethodevanfelix():
+    mydb = mysql.connector.connect(
+        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
+        user="yc2403admin",
+        password="abcd1234ABCD!@#$",
+        database="demopythondag"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM recept")
+
+    myresult = mycursor.fetchall()
+    eindstring = ""
+    for x in myresult:
+        print(x[1], " - ", x[3])
+        eindstring += x[1]+", "
+    return "yes: " + eindstring
