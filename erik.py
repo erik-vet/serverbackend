64188d3b8c56d6923d8b04358fea4ad5210934a1
erik-vet <erikvet1@gmail.com>
2024-03-27 14:00:38 +0100

    derde

--- erik.py
+++ erik.py
@@ -1,4 +1,23 @@
 import mysql.connector
+def methodeerik2():
+    mydb = mysql.connector.connect(
+        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
+        user="yc2403admin",
+        password="abcd1234ABCD!@#$",
+        database="demopythondag"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM recept")
+
+    myresult = mycursor.fetchall()
+    eindstring = ""
+    for x in myresult:
+        print(x[1], " - ", x[3])
+        eindstring += x[1]+", "
+    return "yes: " + eindstring
+
 
 def methodeerik(tweeword, driewoord):
     mydb = mysql.connector.connect(
