b60be3e35a6a39355a1e1e98e0d187482490508f
erik-vet <erikvet1@gmail.com>
2024-03-27 15:38:43 +0100

    opschonen

--- erik.py
+++ erik.py
@@ -1,11 +1,22 @@
-import mysql.connector
+
+
+import onzepython
+
+def methodeerik(tweeword, driewoord):
+    mydb = onzepython.mydb
+
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO recept (naam, aantalsterren) VALUES (%s, %s)"
+    val = (tweeword, driewoord)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+    return "Je recept is aangemaakt!" 
+
 def methodeerik2():
-    mydb = mysql.connector.connect(
-        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
-        user="yc2403admin",
-        password="abcd1234ABCD!@#$",
-        database="demopythondag"
-    )
+    mydb = onzepython.mydb
 
     mycursor = mydb.cursor()
 
@@ -18,21 +29,3 @@ def methodeerik2():
         eindstring += x[1]+", "
     return "yes: " + eindstring
 
-
-def methodeerik(tweeword, driewoord):
-    mydb = mysql.connector.connect(
-        host="yc2403allpurpose.mysql.database.azure.com",  #port erbij indien mac
-        user="yc2403admin",
-        password="abcd1234ABCD!@#$",
-        database="demopythondag"
-    )
-
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO recept (naam, aantalsterren) VALUES (%s, %s)"
-    val = (tweeword, driewoord)
-    mycursor.execute(sql, val)
-
-    mydb.commit()
-
-    print(mycursor.rowcount, "record inserted.")
-    return "Je recept is aangemaakt!" 
